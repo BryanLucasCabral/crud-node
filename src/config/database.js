d575cd182aba50b7ed2b317b5a7ce9399c9b0582
Joseane Nascimento <bryan.brasil.bl@gmail.com>
2022-09-16 19:19:28 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports ={
+    dialect: 'postgres',
+    host:'localhost',
+    port: 5432,
+    username:'root',
+    password:'',
+    database:'crud-node',
+    define:{
+        timestamp: true,
+        undescored: true,
+    }
+}
