169999d4eaefe0a7ac4654d9742d266f0665d979
Joseane Nascimento <bryan.brasil.bl@gmail.com>
2022-09-16 19:58:20 -0300

    servidor rodando na porta 3000

--- src/database/index.js
+++ src/database/index.js
@@ -3,8 +3,8 @@ const configDB = require('../config/database')
 
 const connection = new Sequelize(configDB)
 
-/*connection.authenticate().then(()=>{
+connection.authenticate().then(()=>{
     console.log("conectado com o banco de dados!")
-})*/
+})
 
 module.exports = connection
