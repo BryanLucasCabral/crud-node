e244d0a34bce9af2e12ed4121c5aca75f9c1f575
Bryan Lucas Cabral Do Nascimento <bryan.brasil.bl@gmail.com>
2022-09-16 22:13:35 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const routes = require('./routes')
 const app = express()
-/*const port = 3000*/
+const port = 3000
 
 
 
@@ -10,5 +10,4 @@ require('./database')
 
 app.use(express.json())
 app.use(routes)
-/*app.listen(port, () => {console.log(`aplicacao rodando em http://localhost:${port}`)})*/
-app.listen(3030)
+app.listen(port, () => {console.log(`aplicacao rodando em http://localhost:${port}`)})
